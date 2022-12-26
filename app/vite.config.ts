5d0f0e85cb86b80d4c836cc8a7b512c9e711517a
Oliver Bøving <oliver@bvng.dk>
2022-12-26 14:32:06 +0100

    Add coi-serviceworker to fix COI issues on GitHub Pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import crossOriginIsolated from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "mint",
-  plugins: [react(), crossOriginIsolated()],
+  base: "/mint",
+  plugins: [react()],
 });
